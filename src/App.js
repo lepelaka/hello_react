6e33fa03514c3f79b1461d8b5da55234602feae3
lepelaka <lepelaka@gmail.com>
2023-05-08 12:34:10 +0900

    23/05/08 #3

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
-import Say from "./Say";
+import Scrollbox from "./Scrollbox.js";
 
-const App = () => {
-  return <Say />;
-};
+import React, { Component } from "react";
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Scrollbox ref={(ref) => (this.scrollbox = ref)} />
+        <button onClick={() => this.scrollbox.scrollTo()}>맨 위로</button>
+        <button onClick={() => this.scrollbox.scrollToBottom()}>
+          맨 밑으로
+        </button>
+      </div>
+    );
+  }
+}
 
 export default App;
